cf7da731d1eda52e90bd4dcb122175b2559b7dc3
Guillaume Aubert <gui.auber@gmail.com>
2020-07-10 16:57:50 +0100

    exercice finished

--- pandas_lab.py
+++ pandas_lab.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import requests
+import xlrd
 #CSV
 cols = ['time', 'rad_flow', 'fpv_close', 'fpv_open', 'high', 'bypass', 'bpv_close', 'bpv_open', 'class']
 tst_url ="https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/shuttle/shuttle.tst"
@@ -17,5 +18,4 @@ astronaute.head()
 pd.value_counts(astronaute['Undergraduate Major'])
 print(pd.value_counts(astronaute['Undergraduate Major']))
 
-astronaute.to_csv('/Users/guillaumeaubert/Documents/Ironhack/Module1/Week2/Day3_Pandas/lab-import-export/your-code/astronaute.csv',sep='    ',index=False)
-
+astronaute.to_csv('/Users/guillaumeaubert/Documents/Ironhack/Module1/Week2/Day3_Pandas/lab-import-export/your-code/astronaute.csv',sep=',',index=False)
